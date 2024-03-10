e500e76b6fc4815bdb38e9d657907a3abffd0b4e
itsstevenliu <151034963+itsstevenliu@users.noreply.github.com>
2024-03-10 12:15:12 +1100

    Create availablePathToMoveFolder.dart

---
+++ filemanager/availablePathToMoveFolder.dart
@@ -0,0 +1,45 @@
+import 'dart:convert';
+import 'dart:math' as math;
+
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:intl/intl.dart';
+import 'package:timeago/timeago.dart' as timeago;
+import '/flutter_flow/lat_lng.dart';
+import '/flutter_flow/place.dart';
+import '/flutter_flow/uploaded_file.dart';
+import '/flutter_flow/custom_functions.dart';
+import '/backend/backend.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import '/auth/firebase_auth/auth_util.dart';
+
+List<String> availablePathToMoveFolder(
+  List<String> parentPath,
+  List<String> currentFolderName,
+  String currentParentPath,
+  String fileTitle,
+) {
+  /// MODIFY CODE ONLY BELOW THIS LINE
+
+  List<String> combinedList = [];
+
+  for (int i = 0; i < parentPath.length && i < currentFolderName.length; i++) {
+    String combinedString = '${parentPath[i]}${currentFolderName[i]}/';
+    combinedList.add(combinedString);
+  }
+  combinedList.add('home/');
+
+  Set<String> uniqueSet = Set<String>.from(combinedList);
+  List<String> uniqueCombinedList = uniqueSet.toList();
+
+  // Remove currentParentPath from uniqueCombinedList
+  uniqueCombinedList.remove(currentParentPath);
+
+  // Remove outputs starting with currentParentPath + currentFolderName
+  uniqueCombinedList.removeWhere(
+      (output) => output.startsWith('$currentParentPath${fileTitle}'));
+
+  return uniqueCombinedList;
+
+  /// MODIFY CODE ONLY ABOVE THIS LINE
+}
