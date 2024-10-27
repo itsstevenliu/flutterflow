a8386611fa2f17738b37333452d0fbffd84b4b13
Steven NoCode <151034963+itsstevenliu@users.noreply.github.com>
2024-10-27 15:08:10 +1100

    Update myScheduledCloudFunction.js

--- general/myScheduledCloudFunction.js
+++ general/myScheduledCloudFunction.js
@@ -1,8 +1,8 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 
-// At every minute.
-exports.myScheduledCloudFunction = functions.pubsub.schedule('* * * * *').timeZone('Australia/Sydney').onRun(async (context) => {
+// At every minute. set the timezone you want to run the cloud function based on. hourly is 0 * * * * 
+exports.myScheduledCloudFunction = functions.pubsub.schedule('* * * * *').timeZone('Australia/Sydney').onRun(async (context) => { 
   console.log("CRON job started running");
   // insert own code to do whatever you want
   console.log("CRON job finished");
